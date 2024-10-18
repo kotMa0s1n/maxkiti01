38c5630d1a0f31312df16033f63c23c348198f13
kotMa0s1n <maksimskiba158@gmail.com>
2024-10-18 15:08:25 +0300

    Update 3D render.lua

--- 3D render.lua
+++ 3D render.lua
@@ -1,3 +1,4 @@
+messagebox('By: maxkiti01    t.me/+gyH9PbgbmdwwMTky', "Script active", 0)
 local args = _E and _E.ARGS or {}
 local SET_DISABLED = args[1]
 if SET_DISABLED == nil then SET_DISABLED = not _G.rndr_dis end
