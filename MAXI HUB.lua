0437d66e92becedfad4fa6bbe69975c8722a4af3
kotMa0s1n <maksimskiba158@gmail.com>
2024-12-19 07:02:43 +0300

    Update v3 and rename MAX HUB.lua to MAXI HUB.lua

--- MAXI HUB.lua
+++ MAXI HUB.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("maxkikiti01 multi-hub", "Serpent")
+local Window = Library.CreateLib("MAXI HUB", "Serpent")
 
 
 -------------------------------------- Tab -----------------------------------------------------------------
