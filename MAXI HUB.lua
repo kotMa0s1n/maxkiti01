54754be0e4f63ad2568c097d71de49695f5366ae
kot_ma0s1n <maksimskiba158@gmail.com>
2025-01-04 14:11:52 +0300

    MAXI HUB v1.6.8b

--- MAXI HUB.lua
+++ MAXI HUB.lua
@@ -1,145 +1,214 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("MAXI HUB v1.6.5", "Serpent")
+local Window = Library.CreateLib("MAXI HUB v1.6.8b", "Serpent")
 
 
 -------------------------------------- Tab -----------------------------------------------------------------
 --local Tab = Window:NewTab("Game Scripts")
 local Tab = Window:NewTab("Scripts")
-
 -------------------------------------- Game Scripts ---------------------------------------------------------
 local Section = Tab:NewSection("Game Scripts")
-Section:NewButton("Fisch", "Speed Hub v5", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
-end)
-Section:NewButton("Funky Friday", "beep beep bobobobo beep beep", function()
-    loadstring(game:HttpGet('https://pastebin.com/raw/dcyuEgyK'))()
-end)
-Section:NewButton("Epic Minigames", "Cheat All level", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/SlamminPig/rblxgames/main/Epic%20Minigames/EpicMinigamesGUI"))()
-end)
+
+
 Section:NewButton("Arsenal (BEEEEEEEEEST)", "90% ban )", function()
     loadstring(game:HttpGet('https://pastebin.com/raw/8ysy7ENG',true))()
 end)
-Section:NewButton("1️⃣ Blox Fruits", "MUKURO", function()
+
+
+Section:NewButton("1️⃣ Blox Fruits", "By:  MUKURO", function()
     loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()
 end)
-Section:NewButton("2️⃣ Blox Fruit (best)", "c-a-t-s-u-s", function()
+Section:NewButton("2️⃣ Blox Fruit (best)", "By:  c-a-t-s-u-s", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/3345-c-a-t-s-u-s/Kncrypt/refs/heads/main/sources/BloxFruit.lua"))()
 end)
-Section:NewButton("3️⃣ Blox Fruit", "Speed Hub", function()
+Section:NewButton("3️⃣ Blox Fruit", "By:  Speed Hub", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
 end)
-Section:NewButton("Starving Artist", "Copy Art", function()
-    loadstring(game:HttpGet("https://pastebin.com/raw/aCFfd9Uq"))()
-end)
-Section:NewButton("Bed Wars", "VAPE V4", function()
+
+
+Section:NewButton("Bed Wars", "By:  VAPE V4", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/cocotv666/Aurora/main/Aurora_Loader"))()
 end)
-Section:NewButton("Break In 2", "Starry", function()
+
+Section:NewButton("Break In 2", "By:  Starry", function()
     getgenv().ignoreGameCheck = false;
     loadstring(game:HttpGet("https://luau.tech/build"))()
 end)
-Section:NewButton("Murder Mystery 2", "ESP (no gun)", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Ihaveash0rtnamefordiscord/Releases/main/MurderMystery2HighlightESP"))(' Watermelon ?')
+
+Section:NewButton("Brookhaven", "By:  DarkoneMarcillise", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/TheDarkoneMarcillisePex/Other-Scripts/main/Brook%20Haven%20Gui'))()
 end)
-Section:NewButton("Jailbarke", "Auto Farm", function()
-    loadstring(game:HttpGet('http://scripts.projectauto.xyz/AutoRobV5'))()
+
+Section:NewButton("Build a Boat for Treasure (Only ) (Best)", "By: ???", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/catblox1346/BBHscript/main/owo"))()
 end)
+
+Section:NewButton("Build a Boat for Treasure -- AUTO-FARM (Only PC) (Best)", "Real Best", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Waza80/scripts-new/main/BuildABoat.lua"))("t.me/+gyH9PbgbmdwwMTky")
+end)
+
+Section:NewButton("Creatures of Sonaria", "By: Lunar", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Mangnex/Lunar-Hub/refs/heads/main/FreeLoader.lua"))()
+end)
+
 Section:NewButton("Driving Empire", "Boost Car", function()
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d8bf54daa5b358826ce74cab275f9135.lua"))()
 end)
-Section:NewButton("Gun Grounds FFA", "agreed69", function()
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/agreed69-scripts/open-src-scripts/refs/heads/main/Gun%20Grounds%20FFA.lua")()
+
+Section:NewButton("Epic Minigames", "Cheat All level", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SlamminPig/rblxgames/main/Epic%20Minigames/EpicMinigamesGUI"))()
 end)
-Section:NewButton("Nanny", "Zephyr", function()
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/Unknownlodfc/Zephyr/refs/heads/main/Zephyr%20%7C%20Nanny")()
-end)
-Section:NewButton("Prison Life", "Zephyr", function()
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/Unknownlodfc/Zephyr/refs/heads/main/Zephyr%20%7C%20Prison%20Life")()
-end)
-Section:NewButton("1️⃣ Lumber Tycoon 2 (NO SOLARA)", "LuaWare", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/frencaliber/LuaWareLoader.lw/main/luawareloader.wtf",true))()
-end)
-Section:NewButton("2️⃣ Lumber Tycoon 2", "Better Hub", function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/Butter/main/Root2.lua'))()
-end)
-Section:NewButton("3️⃣ Lumber Tycoon 2", "Kron", function()
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/DevKron/Kron_Hub/refs/heads/main/version_1.0")()
-end)
-Section:NewButton("Creatures of Sonaria", "Lunar", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Mangnex/Lunar-Hub/refs/heads/main/FreeLoader.lua"))()
-end)
-Section:NewButton("Emergency Hamburg", "AirFlow", function()
+
+Section:NewButton("Emergency Hamburg", "By: AirFlow", function()
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d8bf54daa5b358826ce74cab275f9135.lua"))()
 end)
-Section:NewButton("Fling Things and People | Key: ---------->", "Bliz_T", function()
+
+Section:NewButton("Fisch", "By: Speed Hub v5", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+end)
+
+Section:NewButton("Funky Friday", "beep beep bobobobo beep beep", function()
+    loadstring(game:HttpGet('https://pastebin.com/raw/dcyuEgyK'))()
+end)
+
+Section:NewButton("Fling Things and People", "By: Bliz_T", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/BlizTBr/scripts/main/FTAP.lua"))()
 end)
-Section:NewButton("Brookhaven", "DarkoneMarcillise", function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/TheDarkoneMarcillisePex/Other-Scripts/main/Brook%20Haven%20Gui'))()
+
+Section:NewButton("Gun Grounds FFA", "By:  agreed69", function()
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/agreed69-scripts/open-src-scripts/refs/heads/main/Gun%20Grounds%20FFA.lua")()
 end)
-Section:NewButton("Taxi Boss", "???", function()
+
+Section:NewButton("Jailbarke", "Auto Farm", function()
+    loadstring(game:HttpGet('http://scripts.projectauto.xyz/AutoRobV5'))()
+end)
+
+
+Section:NewButton("1️⃣ Lumber Tycoon 2 (Only phone)", "By:  LuaWare", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/frencaliber/LuaWareLoader.lw/main/luawareloader.wtf",true))()
+end)
+Section:NewButton("2️⃣ Lumber Tycoon 2", "By:  Better Hub", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/Butter/main/Root2.lua'))()
+end)
+Section:NewButton("3️⃣ Lumber Tycoon 2", "By:  Kron", function()
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/DevKron/Kron_Hub/refs/heads/main/version_1.0")()
+end)
+
+
+Section:NewButton("Murder Mystery 2", "ESP (no gun)", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Ihaveash0rtnamefordiscord/Releases/main/MurderMystery2HighlightESP"))(' Watermelon ?')
+end)
+
+Section:NewButton("Nanny", "By:  Zephyr", function()
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/Unknownlodfc/Zephyr/refs/heads/main/Zephyr%20%7C%20Nanny")()
+end)
+
+Section:NewButton("Prison Life", "By:  Zephyr", function()
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/Unknownlodfc/Zephyr/refs/heads/main/Zephyr%20%7C%20Prison%20Life")()
+end)
+
+Section:NewButton("Starving Artist", "Copy Art", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/aCFfd9Uq"))()
+end)
+
+Section:NewButton("Taxi Boss", "By:  ???", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/loadstringguy/taxi-boss/refs/heads/main/script.lua"))()
 end)
+
+
+
+
 ------------------------------------ Extra scripts -------------------------------------------
 local Tab = Window:NewTab("Other Scripts")
 local Section = Tab:NewSection("Other Scripts")
-Section:NewButton("Visual", "1\4 rivals :3", function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Actyrn/Scripts/main/AzureModded"))()
-end)
+
+
 Section:NewButton("Aim (NOT AIM BOT)", "All games", function()
     loadstring(game:HttpGet"https://raw.githubusercontent.com/HtB1526/ArceusX_Scripts/refs/heads/main/Scripts/easy_crosshair.lua")()
 end)
+
 Section:NewButton("Aim Bot (Key✅)", "https://discord.gg/hKN3QDYCV8", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxMasterAyla/AzureModdedUniversal/refs/heads/main/AzureModded.lua"))()
 end)
+
 Section:NewButton("Aim Bot (Key❌)", "Auto Clicker, ESP & More", function()
     loadstring(game:HttpGet("https://pastebin.com/raw/b8QDUqJ8"))()
 end)
-Section:NewButton("IY_FE", "Best universal script", function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
-end)
+
 Section:NewButton("Anti-AFK", "No kick afk", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/kotMa0s1n/maxkiti01/refs/heads/main/anti-afk.lua"))()
 end)
+
+Section:NewButton("IY_FE", "Best universal script", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end)
+
 Section:NewButton("1️⃣ Invisible (ONLY PC) (It doesn't work well)", "Press X to become invisible", function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/kotMa0s1n/maxkiti01/refs/heads/main/Inviz.lua'))()
 end)
-Section:NewButton("2️⃣ Button Invisible (NO PRESS X, PLS)", "GUI button", function()
+
+Section:NewButton("2️⃣ Invisible Button (NO PRESS X, PLS)", "GUI button", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Luarmor123/community-Y-HUB/refs/heads/main/invisible-button"))()
 end)
+
+Section:NewButton("Visual shit", "1\4 rivals :3", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Actyrn/Scripts/main/AzureModded"))()
+end)
+
 
 --------------------------------------- Universal -------------------------------------------------
 local Tab = Window:NewTab("Universal")
 local Section = Tab:NewSection("Universal")
+
+
 Section:NewSlider("WalkSpeed", "Changes WalkSpeed", 500, 16, function(v) -- 500 (MaxValue) | 0 (MinValue)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = v
 end)
+
 Section:NewSlider("JumpPower", "Changes JumpPower", 500, 50, function(v) -- 500 (MaxValue) | 0 (MinValue)
     game.Players.LocalPlayer.Character.Humanoid.JumpPower = v
     game.Players.LocalPlayer.Character.Humanoid.JumpHeight = v
 end)
+
+local Section = Tab:NewSection("Universal Script")
+
+
 Section:NewButton("AnitCheat Bypass", "Works in most games but not all", function()
     Events = true -- Allow RemoteEvents (if true, blocks if false)
     Requests = false -- Allow RemoteFunctions (if true, blocks if false)
     Hitbox = true -- Allow Normal Hitbox (if true, attempt to bypass hbe if false)
     loadstring(game:HttpGet("https://pastebin.com/raw/MZrwt5Rm", true))()
-    end)
+end)
+
 Section:NewButton("UNC Check", "Checks UNC of your injector", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/kotMa0s1n/maxkiti01/refs/heads/main/UNC.lua"))()
 end)
 
+
+
+
 ------------------------------------------------- Settings -----------------------------------------------------
 local Tab = Window:NewTab("Settings")
-local Section = Tab:NewSection("Settings")
+local Section = Tab:NewSection("Settings close")
+
+
 Section:NewKeybind("Toggle UI", "Toggle's UI", Enum.KeyCode.RightShift, function()
 	Library:ToggleUI()
 end)
 
+
+
+
 --------------------------------------------- Credits ------------------------------
 local Tab = Window:NewTab("Credits")
 local Section = Tab:NewSection("Credits")
+
+
 Section:NewButton("Made By maxkiti01", "kot_ma0s1n", function()
     local Babo = "https://funpay.com/users/6412543/"
 setclipboard(tostring(Babo))
+end)
+
+Section:NewButton("Helper To0Gts", "<3", function()
+    local Bab = "https://t.me/To0Gts"
+setclipboard(tostring(Bab))
 end)
