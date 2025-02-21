e6b3e34117b1f15a3157a82d4aa87edffea0b2ff
kot_ma0s1n <maksimskiba158@gmail.com>
2025-02-21 19:26:51 +0300

    Delete MAXI HUB ADM.lua

--- MAXI HUB ADM.lua
+++
@@ -1,2 +0,0 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("MAXI HUB ADMIN maxkiti01😸", "Serpent")
